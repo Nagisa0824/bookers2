c11b6f90314aae4e224487aa1c41a7b93f824857
Nagisa0824 <yuzutea0824@gmail.com>
2021-04-26 13:10:56 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,53 @@
 class BooksController < ApplicationController
 
-  def new
-  end
-  
   def create
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+       flash[:notice] = "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-      
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
-      
+    @book = Book.new
+    @mybook = Book.find(params[:id])
+    @user = @mybook.user
   end
 
   def edit
-    
+    @mybook = Book.find(params[:id])
+    if @mybook.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
-    
+    @mybook = Book.find(params[:id])
+  if @mybook.update(book_params)
+    redirect_to book_path(@mybook.id)
+    flash[:notice] = "You have update book successfully."
+  else
+    render :edit
+  end
   end
 
   def destroy
-    
+    @mybook = Book.find(params[:id])
+    @mybook.destroy
+    redirect_to books_path
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,38 @@
 class UsersController < ApplicationController
  def create
-   
  end
 
  def index
-    
+  @users = User.all
+  @book = Book.new
+  @user = current_user
  end
 
  def show
-     
+  @book = Book.new
+  @user = User.find(params[:id])
+  @books = @user.books
  end
 
  def edit
-     
+  @user = User.find(params[:id])
+  if @user == current_user
+      render "edit"
+  else
+      redirect_to user_path(current_user.id)
+  end
  end
 
  def update
-   
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to user_path(@user.id)
+    flash[:notice] = "You have update user successfully."
+  else
+    User.find(params[:id])
+    render :edit
+  end
+
  end
 
  private
